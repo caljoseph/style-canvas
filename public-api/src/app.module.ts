6a72885588a23d83e62128d236a3949dc4da1cb8
Caleb Bradshaw <therealcalebbradshaw@gmail.com>
2024-06-19 17:07:22 -0600

    Integrate with cognito for register endpoint

--- public-api/src/app.module.ts
+++ public-api/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ConfigModule} from "@nestjs/config";
+import {config} from "rxjs";
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({
+        envFilePath: '.env',
+        isGlobal: true,
+      }),
+      AuthModule,
+      UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
