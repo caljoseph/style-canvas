1392277fa3c4e47e71bdfd88b312cae664b284f0
Caleb Bradshaw <therealcalebbradshaw@gmail.com>
2024-12-03 22:48:33 -0700

    fix bug in ml-server-offline.exception

--- public-api/src/image/exceptions/ml-server-offline.exception.ts
+++ public-api/src/image/exceptions/ml-server-offline.exception.ts
@@ -1,7 +1,13 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class MLServerOfflineException extends HttpException {
-    constructor() {
-        super('ML server is currently offline or unreachable', HttpStatus.SERVICE_UNAVAILABLE);
+    constructor(message?: string) {
+        if (typeof message !== 'undefined') {
+            super(message, HttpStatus.SERVICE_UNAVAILABLE);
+
+        } else {
+            super('ML server is currently offline or unreachable', HttpStatus.SERVICE_UNAVAILABLE);
+
+        }
     }
 }
