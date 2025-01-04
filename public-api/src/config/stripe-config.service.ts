a3d8267049c7b2aed76267a22c035500371611b8
Caleb Bradshaw <therealcalebbradshaw@gmail.com>
2025-01-04 16:32:58 -0700

    change stripe key to live

--- public-api/src/config/stripe-config.service.ts
+++ public-api/src/config/stripe-config.service.ts
@@ -4,6 +4,6 @@ import Stripe from "stripe";
 @Injectable()
 export class StripeConfigService {
     public getStripeClient() {
-        return new Stripe(process.env.TEST_STRIPE_SECRET_KEY)
+        return new Stripe(process.env.LIVE_STRIPE_SECRET_KEY)
     }
 }
